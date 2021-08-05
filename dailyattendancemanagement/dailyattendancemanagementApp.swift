d5e7a97e52e707b2f5661705d5f74f1330819446
서창열 <kongbaguni@gmail.com>
2021-08-05 17:28:05 +0900

    Initial Commit

---
+++ dailyattendancemanagement/dailyattendancemanagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  dailyattendancemanagementApp.swift
+//  dailyattendancemanagement
+//
+//  Created by Changyeol Seo on 2021/08/05.
+//
+
+import SwiftUI
+
+@main
+struct dailyattendancemanagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
